24e33260e2898e6b430476130c01fd59ed19ba46
Webshell Learning Tutorial 2024 <168400951+cyberlanciauu26@users.noreply.github.com>
2024-11-20 00:20:42 +0700

    Update header.php

--- header.php
+++ header.php
@@ -237,5 +237,145 @@
 <div><a href="https://yayasan-trisakti.id/">https://yayasan-trisakti.id/</a></div>
 </div>
 
+<div style="display: none;">
+<a href="https://chsz.biz/">mega4d</a>
+<a href="https://gurupendidikan.co.id/">mega4d</a>
+<a href="https://oshikoto-rc.org/">mega4d</a>
+<a href="https://aleppo-cult.org/">mega4d</a>
+<a href="https://www.ibupintargopay.com/">mega4d</a>
+<a href="https://alonabondarenko.org/">mega4d</a>
+<a href="https://www.amicideimusei.org/">mega4d</a>
+<a href="http://astraviec.org/">mega4d</a>
+<a href="https://www.bdtlb.org/">mega4d</a>
+<a href="https://beautynetworkindia.com/">mega4d</a>
+<a href="https://www.boisleux-saint-marc.com/">mega4d</a>
+<a href="https://caledonia-village.com/">mega4d</a>
+<a href="https://certapropainterscomplaints.com/">mega4d</a>
+<a href="https://chagosconservationtrust.org/">mega4d</a>
+<a href="https://citasonlinegratis.com/">mega4d</a>
+<a href="https://codeliverance.org/">mega4d</a>
+<a href="https://diocesisdetacambaro.mx/">mega4d</a>
+<a href="https://directyourpower.com/">mega4d</a>
+<a href="https://www.disbudparmaluku.org/">mega4d</a>
+<a href="https://www.frightnightsky.com/">mega4d</a>
+<a href="https://www.gmniyogyakarta.com/">mega4d</a>
+<a href="https://gobibrighton.com/">mega4d</a>
+<a href="https://www.hhsnopek.com/">mega4d</a>
+<a href="https://www.homeguardsales.com/">mega4d</a>
+<a href="https://ian-harding.org/">mega4d</a>
+<a href="https://jayesun4uscongress.com/">mega4d</a>
+<a href="https://www.jessicabiggsdesign.com/">mega4d</a>
+<a href="https://kesultananbanjar.com/">mega4d</a>
+<a href="https://kofcwhiteakeragency.com/">mega4d</a>
+<a href="https://www.lasberlanas.com/">mega4d</a>
+<a href="https://moamie.com/">mega4d</a>
+<a href="https://www.mresidencejogja.com/">mega4d/a>
+<a href="https://nomadinparis.com/">mega4d</a>
+<a href="https://www.opstinabijeljina.com/">mega4d</a>
+<a href="https://www.paparazzieyeinthedark.com/">mega4d</a>
+<a href="https://profimatics.com/">mega4d</a>
+<a href="https://purbakalajawatengah.org/">mega4d</a>
+<a href="https://www.roslynflowers.com/">mega4d</a>
+<a href="https://www.saintgermaindemarencennes.org/">mega4d</a>
+<a href="https://santamariadelaisla.com/">mega4d</a>
+<a href="https://www.senatusjakarta.org/">mega4d</a>
+<a href="https://shought.com/">mega4d</a>
+<a href="http://sporcasinoadres.com/">mega4d</a>
+<a href="https://standupdepok.com/">mega4d</a>
+<a href="https://super-suplementos.com/">mega4d</a>
+<a href="https://thedigitalken.com/">mega4d</a>
+<a href="https://www.thinkbigtaguig.com/">mega4d</a>
+<a href="https://tkohomerevival.com/">mega4d</a>
+<a href="https://tunisianembassyjakarta.com/">mega4d</a>
+<a href="https://undemocracy.org/">mega4d</a>
+<a href="https://virologist.org/">link daftar mega4d</a>
+<a href="https://weareurals.com/">mega4d</a>
+<a href="https://winnersassociation.org/">mega4d</a>
+<a href="https://saint-cyr-la-roche.com/">prediksi togel</a>
+<a href="https://jayesun4uscongress.com/">https://jayesun4uscongress.com/</a>
+
+</div>
+
+
+
+<div style="display: none;">
+<a href="https://chsz.biz/">https://chsz.biz/</a>
+<a href="https://gurupendidikan.co.id/">https://gurupendidikan.co.id/</a>
+<a href="https://oshikoto-rc.org/">https://oshikoto-rc.org/</a>
+<a href="https://aleppo-cult.org/">https://aleppo-cult.org/</a>
+<a href="https://alonabondarenko.org/">https://alonabondarenko.org/</a>
+<a href="https://www.amicideimusei.org/">https://www.amicideimusei.org/</a>
+<a href="http://astraviec.org/">http://astraviec.org/</a>
+<a href="https://www.bdtlb.org/">https://www.bdtlb.org/</a>
+<a href="https://beautynetworkindia.com/">https://beautynetworkindia.com/</a>
+<a href="https://www.boisleux-saint-marc.com/">https://www.boisleux-saint-marc.com/</a>
+<a href="https://caledonia-village.com/">https://caledonia-village.com/</a>
+<a href="https://certapropainterscomplaints.com/">https://certapropainterscomplaints.com/</a>
+<a href="https://chagosconservationtrust.org/">https://chagosconservationtrust.org/</a>
+<a href="https://citasonlinegratis.com/">https://citasonlinegratis.com/</a>
+<a href="https://codeliverance.org/">https://codeliverance.org/</a>
+<a href="https://diocesisdetacambaro.mx/">https://diocesisdetacambaro.mx/</a>
+<a href="https://directyourpower.com/">https://directyourpower.com/</a>
+<a href="https://www.disbudparmaluku.org/">https://www.disbudparmaluku.org/</a>
+<a href="https://www.frightnightsky.com/">https://www.frightnightsky.com/</a>
+<a href="https://www.gmniyogyakarta.com/">https://www.gmniyogyakarta.com/</a>
+<a href="https://gobibrighton.com/">https://gobibrighton.com/</a>
+<a href="https://www.hhsnopek.com/">https://www.hhsnopek.com/</a>
+<a href="https://www.homeguardsales.com/">https://www.homeguardsales.com/</a>
+<a href="https://ian-harding.org/">https://ian-harding.org/</a>
+<a href="https://www.ibupintargopay.com/">https://www.ibupintargopay.com/</a>
+<a href="https://jayesun4uscongress.com/">https://jayesun4uscongress.com/</a>
+<a href="https://www.jessicabiggsdesign.com/">https://www.jessicabiggsdesign.com/</a>
+<a href="https://kesultananbanjar.com/">https://kesultananbanjar.com/</a>
+<a href="https://kofcwhiteakeragency.com/">https://kofcwhiteakeragency.com/</a>
+<a href="https://www.lasberlanas.com/">https://www.lasberlanas.com/</a>
+<a href="https://moamie.com/">https://moamie.com/</a>
+<a href="https://www.mresidencejogja.com/">https://www.mresidencejogja.com/</a>
+<a href="https://nomadinparis.com/">https://nomadinparis.com/</a>
+<a href="https://www.opstinabijeljina.com/">https://www.opstinabijeljina.com/</a>
+<a href="https://www.paparazzieyeinthedark.com/">https://www.paparazzieyeinthedark.com/</a>
+<a href="https://profimatics.com/">https://profimatics.com/</a>
+<a href="https://purbakalajawatengah.org/">https://purbakalajawatengah.org/</a>
+<a href="https://www.roslynflowers.com/">https://www.roslynflowers.com/</a>
+<a href="https://www.saintgermaindemarencennes.org/">https://www.saintgermaindemarencennes.org/</a>
+<a href="https://santamariadelaisla.com/">https://santamariadelaisla.com/</a>
+<a href="https://www.senatusjakarta.org/">https://www.senatusjakarta.org/</a>
+<a href="https://shought.com/">https://shought.com/</a>
+<a href="http://sporcasinoadres.com/">http://sporcasinoadres.com/</a>
+<a href="https://standupdepok.com/">https://standupdepok.com/</a>
+<a href="https://super-suplementos.com/">https://super-suplementos.com/</a>
+<a href="https://thedigitalken.com/">https://thedigitalken.com/</a>
+<a href="https://www.thinkbigtaguig.com/">https://www.thinkbigtaguig.com/</a>
+<a href="https://tkohomerevival.com/">https://tkohomerevival.com/</a>
+<a href="https://tunisianembassyjakarta.com/">https://tunisianembassyjakarta.com/</a>
+<a href="https://undemocracy.org/">https://undemocracy.org/</a>
+<p><a href="https://mega4dweb.id/">https://mega4dweb.id/</a></p>
+<p><a href="https://linkmega4d.id">https://linkmega4d.id</a></p>
+<a href="https://virologist.org/">link daftar mega4d</a>
+<a href="https://weareurals.com/">mega4d</a>
+<a href="https://winnersassociation.org/">mega4d</a>
+<a href="https://saint-cyr-la-roche.com/">prediksi togel</a>
+</div>
+
+<div style="display: none;">
+<a href="https://ilsuonodibologna.org/">situs toto</a>
+<a href="https://www.handballpedia.org/">situs toto</a>
+<a href="https://vylcan-russia.org/">situs toto</a>
+</div>
+
+<div style="display: none;">
+<a href="https://www.buildabetterally.com/">slot 88</a>
+<a href="https://canizardelolivar.com/">matauangslot</a>
+<a href="https://dosco.org/">matauangslot</a>
+<a href="https://villanuevadecampean.com/">matauangslot</a>
+<a href="http://villarroyadelasierra.com/">matauangslot</a>
+</div>
+
+
+<div style="display: none;">
+<div><a href="https://www.unsrat.org/">https://www.unsrat.org/</a></div>
+<div><a href="https://yayasan-trisakti.id/">https://yayasan-trisakti.id/</a></div>
+</div>
+
 
 
